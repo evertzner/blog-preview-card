a8acfc8499e06355da77c3a1dec14d4730402f5d
evertzner <esteban.vertzner@gmail.com>
2024-08-11 00:36:30 +0200

    finished

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,19 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {}
+    extend: {
+      screens: {
+        '2xl': '1440px'
+      },
+      colors: {
+        yellow: '#F4D04E',
+        gray: {
+          500: '#6B6B6B',
+          950: '#111111'
+        },
+        white: '#ffffff'
+      }
+    }
   },
   plugins: []
 };
